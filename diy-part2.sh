f698b94a8abb22566ce0084da257e913df703d6e
GitHub Action <github-actions[bot]@github.com>
2024-03-23 14:04:35 +0000

    Sync files 3064447a23411fff2ff0ca2c3871056f54e0a7b1

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,15 +16,9 @@ sed -i 's/192.168.1.1/192.168.0.2/g' package/base-files/files/bin/config_generat
 # 修改主机名字，把 iStore OS 修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/bin/config_generate
 
-# 更改 Argon 主题背景
-cp -f $GITHUB_WORKSPACE/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-
 # 移除要替换的包
-#rm -rf feeds/packages/net/mosdns
 rm -rf feeds/packages/net/v2ray-geodata
-#cp -r -f ./feeds/第三方源的文件 ./feeds/packages/net/mosdns
 rm -rf feeds/third_party/luci-app-LingTiGameAcc
-rm -rf feeds/third_party/luci-app-pushbot
 rm -rf feeds/third/luci-theme-argon
 
 # Git稀疏克隆，只克隆指定目录到本地
@@ -40,16 +34,18 @@ function git_sparse_clone() {
 # 添加额外插件
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
 git clone https://github.com/innmonkey/luci-theme-argon package/luci-theme-argon
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-adguardhome
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-openclash
 git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-aliddns
+git_sparse_clone master https://github.com/kiddin9/openwrt-packages luci-app-filebrowser filebrowser
 
 # 加入OpenClash核心
 chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
 $GITHUB_WORKSPACE/preset-clash-core.sh
 
+# 更改 Argon 主题背景
+cp -f $GITHUB_WORKSPACE/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 echo "
 # 额外组件
@@ -63,26 +59,19 @@ CONFIG_PACKAGE_luci-app-openclash=y
 CONFIG_PACKAGE_luci-app-adguardhome=y
 
 # mosdns
-CONFIG_PACKAGE_luci-app-mosdns=y
+#CONFIG_PACKAGE_luci-app-mosdns=y
 
 # pushbot
 CONFIG_PACKAGE_luci-app-pushbot=y
 
 # Jellyfin
 CONFIG_PACKAGE_luci-app-jellyfin=y
-CONFIG_PACKAGE_app-meta-jellyfin=y
-CONFIG_PACKAGE_luci-i18n-jellyfin-zh-cn=y
 
 # qbittorrent
 CONFIG_PACKAGE_luci-app-qbittorrent=y
 
 # transmission
 CONFIG_PACKAGE_luci-app-transmission=y
-CONFIG_PACKAGE_transmission-daemon=y
-CONFIG_PACKAGE_luci-i18n-transmission-zh-cn=y
-CONFIG_PACKAGE_app-meta-transmission=y
-CONFIG_PACKAGE_transmission-web-control=y
-CONFIG_PACKAGE_transmission-daemon-openssl=y
 
 # uhttpd
 CONFIG_PACKAGE_luci-app-uhttpd=y
@@ -90,9 +79,7 @@ CONFIG_PACKAGE_luci-app-uhttpd=y
 # 阿里DDNS
 CONFIG_PACKAGE_luci-app-aliddns=y
 
-# rclone
-#CONFIG_PACKAGE_rclone=y
-#CONFIG_PACKAGE_fuse3-utils=y
+# filebrowser
+CONFIG_PACKAGE_luci-app-filebrowser=y
 
 " >> .config
-
