ebe23eb6c5026cba474f3ba2d51fd120ffdb5755
Chris <152150297+innmonkey@users.noreply.github.com>
2024-03-29 17:08:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,8 +17,8 @@ sed -i 's/192.168.1.1/192.168.0.2/g' package/base-files/files/bin/config_generat
 sed -i 's/OpenWrt/iStoreOS/g' package/base-files/files/bin/config_generate
 
 # 移除要替换的包
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/v2ray-geodata
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 rm -rf feeds/third_party/luci-app-LingTiGameAcc
 
 # Git稀疏克隆，只克隆指定目录到本地
